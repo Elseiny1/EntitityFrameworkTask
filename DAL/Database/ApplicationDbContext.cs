39f17c7b8e4b6902104410cc0989cff64fcd0b96
EAGLE <EAGLE@DESKTOP-24AJO1U>
2023-08-21 19:10:43 +0300

    Add project files.

---
+++ DAL/Database/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntitityFrameworkTask.DAL.Entities;
+
+namespace EntitityFrameworkTask.DAL.Database
+{
+    public class ApplicationDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=(localdb)\\MSSQLLocalDB;integrated security=SSPI;initial catalog=EmpTaskEntity;trustservercertificate=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EntitityFrameworkTask
+{
+    public class program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
